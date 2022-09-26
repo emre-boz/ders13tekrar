21d3727c4a3ee29255fd6f80de3735eba05cea3d
Emre Boz <emrento@gmail.com>
2022-09-26 15:23:28 +0300

    outlet yapısı incelendi

---
+++ src/pages/Layout.js
@@ -0,0 +1,33 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section>
+        <Outlet />
+      </section>
+
+      <footer>
+        <p>Tüm hakları saklıdır.</p>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
