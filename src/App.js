2c0770038e9b2b0e9437109ea9315632a0057546
Emre Boz <emrento@gmail.com>
2022-09-26 14:40:26 +0300

    İç içe rooter örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Ekran1 from "./ekranlar/Ekran1";
 import Ekran2 from "./ekranlar/Ekran2";
 import Bulunamadi from "./ekranlar/Bulunamadi";
 import AnaEkran from "./ekranlar/AnaEkran";
 import Header from "./Header";
 function App() {
-  
   return (
     <>
-    <BrowserRouter>
-<Header />
+      <BrowserRouter>
+        <Header />
 
-    <section>
-<p>İçerik bölgesi</p>
-<Routes>
-  <Route path="/" element={<AnaEkran />} />
-  <Route path="/ekran1" element={<Ekran1 />} />
-  <Route path="/ekran2" element={<Ekran2 />} />
-  <Route path="/*" element={<Bulunamadi />} />
-</Routes>
-    </section>
+        <section>
+          <p>İçerik bölgesi</p>
+          <Routes>
+            <Route path="/">
+              <Route index element={<AnaEkran />} />
+              <Route path="ekran1" element={<Ekran1 />} />
+              <Route path="ekran2" element={<Ekran2 />} />
+              <Route path="*" element={<Bulunamadi />} />
+            </Route>
+          </Routes>
+        </section>
 
-    <footer>
-      <p>Ben alt bölgeyim</p>
-    </footer>
-    </BrowserRouter>
+        <footer>
+          <p>Ben alt bölgeyim</p>
+        </footer>
+      </BrowserRouter>
     </>
   );
 }
