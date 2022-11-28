c35452980b60ce0f8e5a911f48ec625e1a05636a
Emre Boz <emrento@gmail.com>
2022-11-28 21:49:17 +0300

    kadin erkek headere eklendi

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,7 @@ function Header() {
           <Link to="/">Ana Ekran</Link>
           <Link to="/ekran1">Ekran 1</Link>
           <Link to="/ekran2">Ekran 2</Link>
+          <Link to="/kategori/kadin">Kadin</Link>
         </nav>
       </header>
     </>
